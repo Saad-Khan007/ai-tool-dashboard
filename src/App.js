e229d3fba5bee8127cdaf421fb15e1a0bf8c42f4
Saad-Khan007 <saadkhan322417@gmail.com>
2023-11-29 20:26:25 +0500

    Brand name changed

--- src/App.js
+++ src/App.js
@@ -179,7 +179,7 @@ export default function App() {
           <Sidenav
             color={sidenavColor}
             brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName="Material Dashboard 2"
+            brandName="AI Tool Dashboard"
             routes={routes}
             onMouseEnter={handleOnMouseEnter}
             onMouseLeave={handleOnMouseLeave}
