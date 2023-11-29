0a239cfaaa4150dbe7570bbd007b402cd87482b1
“[firstname <aliimran98887@gmail.com>
2023-11-29 11:59:27 +0500

    commented

---
+++ src/layouts/dashboard/components/Projects/index.js
@@ -0,0 +1,221 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Icon from "@mui/material/Icon";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import FilterListIcon from '@mui/icons-material/FilterList';
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+// Material Dashboard 2 React examples
+import DataTable from "examples/Tables/DataTable";
+import { func } from "prop-types";
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';
+import 'moment-timezone';
+
+
+
+
+function Projects(props) {
+  const [columns, setColumns] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [menu, setMenu] = useState(null);
+  const [title, setTitle] = useState(null);  
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [selectedFilter, setSelectedFilter] = useState('');
+  const [anchorEl, setAnchorEl] = useState(null);
+
+
+  useEffect(() => {
+     if (props.isAll) {
+      setTitle('Records')
+      setColumns([
+        { Header: "Keyword", accessor: "keyword", width: "40%", align: "left" },
+        { Header: "Email", accessor: "email", width: "40%", align: "left" },
+        { Header: "Ingredients", accessor: "ingredients", width: "20%", align: "left"}
+      ])
+      findAll();
+    }
+  }, []);
+
+  const handleFilterChange = (event) => {
+    const filterValue = event.target.value;
+    setSelectedFilter(filterValue);
+  
+    if (filterValue !== 'Custom') {
+      findAll(filterValue);
+    }
+  };
+  
+  useEffect(() => {
+    findAll(selectedFilter);
+  }, [selectedFilter]);
+
+  
+  const findAll = (filter) => {
+    let start, end;
+    const today = moment().startOf('day').format('YYYY-MM-DD');
+    const startOfThisWeek = moment().startOf('week').format('YYYY-MM-DD');
+    const startOfLastWeek = moment().subtract(1, 'week').startOf('week').format('YYYY-MM-DD');
+    const startOfThisMonth = moment().startOf('month').format('YYYY-MM-DD');
+    const startOfLastMonth = moment().subtract(1, 'month').startOf('month').format('YYYY-MM-DD');
+
+    switch (filter) {
+      case 'Today':
+        start = today;
+        end = moment().endOf('day').format('YYYY-MM-DD');
+        break;
+  
+      case 'Yesterday':
+        start = moment().subtract(1, 'day').startOf('day');
+        end = moment().subtract(1, 'day').endOf('day');
+  
+        break;
+  
+      case 'This Week':
+        start = startOfThisWeek;
+        end = moment().endOf('day');
+        break;
+  
+      case 'Last Week':
+        start = startOfLastWeek;
+        end = moment().subtract(1, 'week').endOf('week');
+        break;
+    
+      case 'This Month':
+        start = startOfThisMonth;
+        end = moment().endOf('day');
+        break;
+  
+      case 'Last Month':
+        start = startOfLastMonth;
+        end = moment().subtract(1, 'month').endOf('month');
+        break;
+        default:
+        
+          break;
+        
+        
+    }
+    
+    axios.post('https://ap-southeast-1.aws.data.mongodb-api.com/app/data-pezxd/endpoint/listall', {
+       startDate: moment(start).format('YYYY-MM-DD'),
+       endDate: moment(end).format('YYYY-MM-DD'),
+        },
+    {
+      'Content-Type': 'application/json'
+    })
+
+      .then(function (response) {
+        displayData(response.data);
+      })
+      .catch(function (error) {   
+      });
+
+  };
+  
+  
+  function displayData(result) {
+    const data = [];
+    result.forEach((item, i) => {
+      
+      if (props.isTodaysdata) {
+        data.push(item)
+      } else if (props.isIngredients) {
+        data.push({ ingredient: item })
+      }
+       else if (props.isAll) {
+        data.push(item)
+      }
+    });
+
+    setRows(data);
+  }
+  const openMenu = ({ currentTarget }) => setMenu(currentTarget);
+  const closeMenu = () => setMenu(null);
+
+  return (
+    <Card>
+      <MDBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
+        <MDBox>
+          <MDTypography variant="h6" gutterBottom>
+            {/* {title} */}
+          </MDTypography>
+        </MDBox>
+      </MDBox>
+
+      {props.isAll ? (
+      <Box 
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          p: 1,
+        }}
+      > 
+      <FormControl style={{ width: '250px', padding: '8px', height: '60px' }}>
+      <Select
+        value={selectedFilter}
+        onChange={handleFilterChange}
+        displayEmpty  
+        style={{ width: '100%', height: '100%' }}
+      >
+        <MenuItem value="" disabled> 
+          Select Filter
+        </MenuItem>
+        <MenuItem value="Today">Today</MenuItem>
+        <MenuItem value="Yesterday">Yesterday</MenuItem>
+        <MenuItem value="This Week">This Week</MenuItem>
+        <MenuItem value="Last Week">Last Week</MenuItem>
+        <MenuItem value="This Month">This Month</MenuItem>
+        <MenuItem value="Last Month">Last Month</MenuItem>
+      </Select>
+    </FormControl>
+        {selectedFilter === 'Custom' && (
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          
+          </Box>
+        )}
+      </Box>
+    ) : null}
+
+      <MDBox>
+        <DataTable
+          table={{ columns, rows }}
+          showTotalEntries={true}
+          isSorted={true}
+          entriesPerPage={5}
+        />
+       
+      </MDBox>
+    </Card>
+  );
+}
+
+export default Projects;
---
+++ src/layouts/dashboard/index.js
@@ -0,0 +1,124 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+
+import { useState, useEffect } from "react";
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import axios from "axios";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+
+// Data
+import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
+import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
+
+// Dashboard components
+import Projects from "layouts/dashboard/components/Projects";
+import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
+
+function Dashboard() {
+  const { sales, tasks } = reportsLineChartData;
+  const [emails, setEmailCount] = useState(0);
+  const [keywords, setKeywordsCount] = useState(0);
+  const [ingred, setingCount] = useState(0);
+  const [total, setTotalCount] = useState(0);
+  const [mostsearchedingredients , setMostsearched]=useState(0);
+
+  useEffect(() => {
+    findAll()
+  }, []);
+
+  function findAll() {
+    axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/data-pezxd/endpoint/stats')
+    .then(function (response) {
+      if (response.data) {
+        setEmailCount(response.data.users.length)
+        setKeywordsCount(response.data.keywords.length)
+        setingCount(response.data.ingredients.length)
+        setTotalCount(response.data.total.length)
+        setMostsearched(response.data.mostSearchedIngredients)
+      }
+    })
+  }
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox py={3}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6} lg={3.6}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                icon="leaderboard"
+                title=" Users"
+                count={emails}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3.6}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="success"
+                icon="leaderboard"
+                title="Ingredients"
+                count={ingred}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3.6}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="success"
+                icon="leaderboard"
+                title="Most Searched Ingredient"
+                count={mostsearchedingredients}
+                countStyle={{ fontSize: '14px' }}
+              />
+            </MDBox>
+          </Grid>
+        </Grid>
+        {/* <MDBox>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={8}>
+              <Projects isTodaysdata={true} />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <Projects isIngredients={true} />
+            </Grid>
+          </Grid>
+        </MDBox> */}
+        <MDBox mt={3}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Projects isAll={true} />
+            </Grid>
+          </Grid>
+        </MDBox>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Dashboard;
