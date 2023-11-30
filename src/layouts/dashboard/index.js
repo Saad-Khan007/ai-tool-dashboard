b1b7d0d2353c55bb599e93cf0ba0fdf4a80fef21
Saad-Khan007 <saadkhan322417@gmail.com>
2023-11-30 18:20:55 +0500

    User List Added

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,54 +1,52 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// @mui material components
 import Grid from "@mui/material/Grid";
-
 import { useState, useEffect } from "react";
-// Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import axios from "axios";
-
-// Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
 import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
 import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
-
-// Data
+import React from "react";
 import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
 import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
-
 // Dashboard components
 import Projects from "layouts/dashboard/components/Projects";
 import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
-
+import Avatar from "@mui/material/Avatar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import PersonIcon from "@mui/icons-material/Person";
+import AddIcon from "@mui/icons-material/Add";
+import { blue } from "@mui/material/colors";
 function Dashboard() {
+  const [openUserDialog, setOpenUserDialog] = React.useState(false);
+  const [selectedUser, setSelectedUser] = React.useState(null);
   const { sales, tasks } = reportsLineChartData;
   const [emails, setEmailCount] = useState(0);
   const [keywords, setKeywordsCount] = useState(0);
+  const [userEmail, setuserEmail] = useState();
   const [ingred, setingCount] = useState(0);
   const [total, setTotalCount] = useState(0);
   const [mostsearchedingredients, setMostsearched] = useState(0);
-
+  const [showUserList, setShowUserList] = useState(false);
   useEffect(() => {
     findAll();
   }, []);
-
+  const handleUserDialogOpen = (user) => {
+    setSelectedUser(user);
+    setOpenUserDialog(true);
+  };
+  const handleUserDialogClose = () => {
+    setSelectedUser(null);
+    setOpenUserDialog(false);
+  };
   function findAll() {
     axios
       .get("https://ap-southeast-1.aws.data.mongodb-api.com/app/data-pezxd/endpoint/stats")
@@ -59,19 +57,21 @@ function Dashboard() {
           setingCount(response.data.ingredients.length);
           setTotalCount(response.data.total.length);
           setMostsearched(response.data.mostSearchedIngredients);
+          setuserEmail(response.data.users);
         }
       });
   }
-
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox py={3}>
         <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={3.6}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard icon="leaderboard" title=" Users" count={emails} />
-            </MDBox>
+          <Grid item xs={12} md={6} lg={3.6} onClick={() => setShowUserList(true)}>
+            <div>
+              <MDBox mb={1.5}>
+                <ComplexStatisticsCard icon="leaderboard" title=" Users" count={emails} />
+              </MDBox>
+            </div>
           </Grid>
           <Grid item xs={12} md={6} lg={3.6}>
             <MDBox mb={1.5}>
@@ -95,27 +95,37 @@ function Dashboard() {
             </MDBox>
           </Grid>
         </Grid>
-        {/* <MDBox>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={8}>
-              <Projects isTodaysdata={true} />
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <Projects isIngredients={true} />
-            </Grid>
-          </Grid>
-        </MDBox> */}
         <MDBox mt={3}>
           <Grid container spacing={3}>
             <Grid item xs={12}>
               <Projects isAll={true} />
             </Grid>
           </Grid>
+          {showUserList && (
+            <MDBox>
+              <Dialog id="user-list" onClose={() => setShowUserList(false)} open={showUserList}>
+                <DialogTitle>User List</DialogTitle>
+                <List sx={{ pt: 0 }}>
+                  {userEmail?.map((email, index) => (
+                    <ListItem disableGutters key={index}>
+                      <ListItemButton>
+                        <ListItemAvatar>
+                          <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                            <PersonIcon />
+                          </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={email} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+              </Dialog>
+            </MDBox>
+          )}
         </MDBox>
       </MDBox>
       <Footer />
     </DashboardLayout>
   );
 }
-
 export default Dashboard;
